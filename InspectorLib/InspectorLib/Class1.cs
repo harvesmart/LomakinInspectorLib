31ab8ccc9ff0a1d44769996afc30830984e63d28
harvesmart <harvesmart@gmail.com>
2024-11-29 06:59:59 +0900

    LomakinInspectorLib

---
+++ InspectorLib/InspectorLib/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InspectorLib
+{
+    public class FunctionInsp
+    {
+        private string CarInspection = "Автоинспекция г. Чита";
+        private string  MainInspector= "Васильев Василий Иванович";
+        private List<string> Workers = new List<string> { "Иванов И.И.", "Зиронов Т.А.", "Миронов А.В.", "Васильев В.И." };
+
+        // Метод возвращает ФИО главного инспектора
+        public string GetInspector()
+        {
+            return MainInspector;
+        }
+
+        // Метод возвращает название автоинспекции
+        public string GetСarInspection()
+        {
+            return CarInspection;
+        }
+
+        // Метод изменяет ФИО главного инспектора (если он есть в списке сотрудников)
+        public bool SetInspector(string fullname)
+        {
+            if (Workers.Contains(fullname))
+            {
+                MainInspector = fullname;
+                return true;
+            }
+            return false;
+        }
+
+        // Метод генерирует госномер в формате SYMBOLnumber_CODE
+        public string GenerateNumber(int number, char symbol, int code = 75)
+        {
+            string formattedSymbol = char.ToUpper(symbol).ToString();
+            return $"{formattedSymbol}{number:D4}_{code:D2}";
+        }
+
+        // Метод возвращает список сотрудников
+        public List<string> GetWorker()
+        {
+            return new List<string>(Workers);
+        }
+
+        // Метод добавляет нового сотрудника
+        public void AddWorker(string fullname)
+        {
+            Workers.Add(fullname);
+        }
+    }
+}
---
+++ InspectorLib/InspectorLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("InspectorLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ReviOS 11 23.12")]
+[assembly: AssemblyProduct("InspectorLib")]
+[assembly: AssemblyCopyright("Copyright © ReviOS 11 23.12 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0b37529e-4e49-4e4d-b98c-3bf64bdd0f6f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
